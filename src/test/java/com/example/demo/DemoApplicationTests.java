cd38e3785c9f0cd7602549e931fadd1351b941fa
MrMeex <mrmeex@localhost.localdomain>
2019-06-06 00:47:50 -0300

    Probando test unitario con Junit

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -9,7 +9,9 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DemoApplicationTests {
-
+	//Esto deberia testear el componente DemoApplication
+	//cada test deberia testear los nuevos componentes que se
+	// creando
 	@Test
 	public void contextLoads() {
 		Assert.assertTrue("Assertion has been false",1==1);
